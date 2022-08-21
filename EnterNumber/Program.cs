53e12357b06b05d8f87be1feb0bb399f227903b6
TheCreator2022 <106817598+TheCreator2022@users.noreply.github.com>
2022-08-20 22:52:31 -0300

    Add files via upload

---
+++ EnterNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace HelloWold
+{
+    public static class Program 
+    {         
+        public static void Main()
+        {
+            Console.WriteLine("Enter A Number:");
+            
+            string outputNumber = Console.ReadLine();
+            
+            Console.WriteLine("Number is " + outputNumber);
+            
+            // The end.
+        }
+    }
+}
