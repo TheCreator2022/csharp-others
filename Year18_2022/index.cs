7955b7f2c746e79c21516cd28a6d4bd2eb6eb7e4
TheCreator2022 <106817598+TheCreator2022@users.noreply.github.com>
2022-08-21 21:32:49 -0300

    Add files via upload

---
+++ Year18_2022/index.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace MyIndex
+{
+    public static class Program 
+    {         
+        public static void Main()
+        {
+            Console.WriteLine("Enter a Color Red:");
+            string colorRed = Console.ReadLine();
+            
+            Console.WriteLine("Enter a Color Green:");
+            string colorGreen = Console.ReadLine();
+            
+            Console.WriteLine("Enter a Color Blue:");
+            string colorBlue = Console.ReadLine();
+            
+            Console.WriteLine("Source CSS");
+            Console.WriteLine("color:rgb(" + colorRed + ", " + colorGreen + ", " + colorBlue + ")");
+        }
+    }
+}
