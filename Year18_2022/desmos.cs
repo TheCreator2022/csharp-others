bbdc6ea8c98f906ce3ee31dde93d1b91b47cee9f
TheCreator2022 <106817598+TheCreator2022@users.noreply.github.com>
2022-08-28 17:22:49 -0300

    Add files via upload

---
+++ Year18_2022/desmos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Desmosnode
+{
+    public static class Desmos 
+    {         
+        public static void Main()
+        {
+        	int i = 0;
+        	string a = "";
+        	Console.WriteLine("Hello!");
+        	
+        	while (i < 97104)
+        	{
+        		Console.WriteLine("Enter a Passive:");
+        		string jat = Console.ReadLine();
+        		a = jat;
+        		Console.WriteLine("RESULTED:" + a);
+        		Console.WriteLine("Input Again!!!");
+        		i++;
+        	}
+        	
+        	Console.WriteLine("i next forgor:desmos 2023.")
+        }
+    }
+}
